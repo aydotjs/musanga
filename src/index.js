6b3fec48fa3e04f52ce874a0ee6e763ebfa5824b
aydotjs <ayobami66@gmail.com>
2024-10-30 09:40:41 +0100

    styled components

--- src/index.js
+++ src/index.js
@@ -4,8 +4,46 @@ import './index.css';
 
 
 const App = () => {
-  return <h1>Hello World, this is the teaching branch</h1>
+  return (
+    <div className='container'>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  )
 }
+
+const Header = () => {
+  return <header className='header'>
+    <h1>Musanga</h1>
+  </header>
+}
+const Menu = () => {
+  return <main className='menu'>
+    <h2>Our Menu</h2>
+    <div className='foods'>
+      <Food />
+    </div>
+
+  </main>
+}
+const Food = () => {
+  return <div className='food'>
+    <img src={'/assets/corn.jpg'} />
+    <div>
+      <h3>Corn</h3>
+      <p>Corn and Cucumber</p>
+    </div>
+
+  </div>
+}
+const Footer = () => {
+  return (
+    <footer className='footer'>We are currently opened</footer>
+  )
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
